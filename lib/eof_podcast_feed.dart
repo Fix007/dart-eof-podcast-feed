852e5f9a5e160d8c475c079f4145fbeadc178a61
Felix <34555911+Fix007@users.noreply.github.com>
2021-01-17 23:53:01 +0100

    show PodcastEpisodeEnclosure

--- lib/eof_podcast_feed.dart
+++ lib/eof_podcast_feed.dart
@@ -1,4 +1,4 @@
 library eof_podcast_feed;
 
 export 'src/podcast.dart' show Podcast;
-export 'src/podcast_episode.dart' show PodcastEpisode;
+export 'src/podcast_episode.dart' show PodcastEpisode, PodcastEpisodeEnclosure;
